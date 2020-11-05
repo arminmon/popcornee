f8d3d8c7f678c03d0641b410105ad11c1d9e86c6
arminmon <arminmon@outlook.com>
2020-11-05 13:51:46 +1100

    outsource vuetify options
    vuetify.options.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,3 @@
-import colors from 'vuetify/es5/util/colors'
 require('dotenv').config()
 
 export default {
@@ -48,36 +47,6 @@ export default {
 			},
 			icons: 'mdi'
 		},
-		theme: {
-			dark: false,
-			themes: {
-				dark: {
-					primary: colors.orange.base,
-					accent: colors.amber.darken3,
-					secondary: colors.grey.darken3,
-					info: colors.cyan.accent4,
-					warning: colors.amber.base,
-					error: colors.deepOrange.accent4,
-					success: colors.green.accent2
-				},
-				light: {
-					primary: colors.orange.darken3,
-					accent: colors.amber.darken4,
-					secondary: colors.grey.lighten4,
-					info: colors.cyan.accent4,
-					warning: colors.amber.base,
-					error: colors.deepOrange.accent4,
-					success: colors.green.accent2
-				}
-			}
-		}
-		// lang: {
-		//   t: (key, ...params) => app.i18n.t(key, params)
-		// }
-	},
-
-	build: {
-		transpileDependencies: ['vuetify']
-		// extend(config, ctx) {}
+		optionsPath: './vuetify.options.js'
 	}
 }
